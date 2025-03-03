c531b940a5e24eacfa5afcf042ecaa0076a0bf64
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-03-03 15:25:13 +0700

    update: implement logging mybuild controller

--- src/controllers/MyBuild.controller.ts
+++ src/controllers/MyBuild.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import { MyBuild } from "@/models/index.ts";
+import logger from "@/config/logger.ts";
 
 export default {
   getMyBuilds: async (req: Request, res: Response) => {
     const { id: userId } = req.user;
+    logger.info("Fetching all builds by userId", { userId });
     try {
       const builds = await MyBuild.findAll({
         where: {
@@ -12,17 +14,23 @@ export default {
       });
 
       if (builds.length === 0) {
+        logger.warn("Builds not found", { userId });
         return res.status(404).json({ error: "Builds not found" });
       }
 
-      res.status(200).json(builds);
+      logger.info("Builds fetched successfully", { builds });
+      return res.status(200).json(builds);
     } catch (error) {
-      res.status(500).json({ message: "Error retrieving builds" });
+      logger.error("Error fetching builds", { error });
+      return res
+        .status(500)
+        .json({ message: "Error retrieving builds", error });
     }
   },
   getMyBuildById: async (req: Request, res: Response) => {
     const { id: userId } = req.user;
     const { id } = req.params;
+    logger.info("Fetching build by ID", { id, userId });
     try {
       const build = await MyBuild.findOne({
         where: {
@@ -31,37 +39,45 @@ export default {
         },
       });
       if (!build) {
-        res.status(404).json({ message: "Build not found" });
+        logger.warn("Build with not found", { id, userId });
+        return res.status(404).json({ message: "Build not found" });
       } else {
-        res.status(200).json(build);
+        logger.warn("Build fetched successfully", { build });
+        return res.status(200).json(build);
       }
     } catch (error) {
-      res.status(500).json({ message: "Error retrieving build" });
+      logger.error("Error fetching build", { error });
+      return res.status(500).json({ message: "Error retrieving build", error });
     }
   },
   createMyBuild: async (req: Request, res: Response) => {
     const { id: userId } = req.user;
     const { name, description } = req.body;
+    logger.info("Creating build", { name, description, userId });
     try {
       if (!name || !description) {
+        logger.warn("Name and description are required");
         return res
           .status(400)
           .json({ message: "Name and description are required" });
       }
-      const build = await MyBuild.create({
+      const _build = await MyBuild.create({
         userId,
         name,
         description,
       });
-      res.status(201).json({ message: "Build created successfully" });
+      logger.info("Build created successfully", { build: _build });
+      return res.status(201).json({ message: "Build created successfully" });
     } catch (error) {
-      res.status(500).json({ message: "Error creating build " + error });
+      logger.error("Error creating build", { error });
+      return res.status(500).json({ message: "Error creating build", error });
     }
   },
   updateMyBuild: async (req: Request, res: Response) => {
     const { id: buildId } = req.params;
     const { id: userId } = req.user;
     const { name, description } = req.body;
+    logger.info("Updating build", { buildId, name, description, userId });
     try {
       const build = await MyBuild.findOne({
         where: { id: buildId, userId },
@@ -71,27 +87,34 @@ export default {
           name,
           description,
         });
-        res.status(200).json({ message: "Build updated successfully" });
+        logger.info("Build updated successfully", { build });
+        return res.status(200).json({ message: "Build updated successfully" });
       } else {
-        res.status(404).json({ message: "Build not found" });
+        logger.warn("Build not found", { buildId, userId });
+        return res.status(404).json({ message: "Build not found" });
       }
     } catch (error) {
-      res.status(500).json({ message: "Error updating build" });
+      logger.error("Error updating build", { error });
+      return res.status(500).json({ message: "Error updating build ", error });
     }
   },
   deleteMyBuild: async (req: Request, res: Response) => {
     const { id: buildId } = req.params;
     const { id: userId } = req.user;
+    logger.info("Deleting build", { buildId, userId });
     try {
       const build = await MyBuild.findOne({ where: { id: buildId, userId } });
       if (build) {
         await build.destroy();
-        res.status(200).json({ message: "Build successfully deleted" });
+        logger.info("Build deleted successfully", { build });
+        return res.status(200).json({ message: "Build successfully deleted" });
       } else {
-        res.status(404).json({ message: "Build not found" });
+        logger.warn("Build not found", { buildId, userId });
+        return res.status(404).json({ message: "Build not found" });
       }
     } catch (error) {
-      res.status(500).json({ message: "Error deleting build" });
+      logger.error("Error deleting build", { error });
+      return res.status(500).json({ message: "Error deleting build", error });
     }
   },
 };
