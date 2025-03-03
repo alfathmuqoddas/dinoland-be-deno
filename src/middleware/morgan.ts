00f8edbb95d81c9e311b73f3496f4130f9bc10e5
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-03-03 15:26:09 +0700

    update: change morgan http status from string to number

--- src/middleware/morgan.ts
+++ src/middleware/morgan.ts
@@ -2,20 +2,21 @@ import morgan, { StreamOptions } from "morgan";
 import logger from "@/config/logger.ts";
 import { Request, Response } from "express";
 
+// Define the stream for logging (Winston integration)
 const stream: StreamOptions = {
   write: (message: string) => {
     logger.info(JSON.parse(message)); // Send logs to Winston
   },
 };
 
-// Custom Morgan format (ELK-friendly JSON)
+// Define a custom Morgan format (ELK-friendly JSON)
 const morganMiddleware = morgan(
   (tokens: any, req: Request, res: Response) => {
     return JSON.stringify({
       timestamp: tokens.date(req, res, "iso"),
       method: tokens.method(req, res),
       url: tokens.url(req, res),
-      status: tokens.status(req, res),
+      status: Number(tokens.status(req, res)), // Ensure it's a number
       responseTime: `${tokens["response-time"](req, res)} ms`,
       userAgent: req.headers["user-agent"],
     });
